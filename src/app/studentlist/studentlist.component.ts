05d3a301b579119f19504ea210f57a8a24e0a2c1
LAPTOP-N7M199B9\pc <ikaveena@gmail.com>
2021-07-01 14:09:35 +0530

    api call

---
+++ src/app/studentlist/studentlist.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import studentData from '../student.json';
+
+interface Student{
+  id: Number;
+  name: String;
+  email: String;
+  gender: String;
+}
+
+@Component({
+  selector: 'app-studentlist',
+  templateUrl: './studentlist.component.html',
+  styleUrls: ['./studentlist.component.css']
+})
+export class StudentlistComponent implements OnInit {
+
+  constructor() { }
+  students: Student[]=studentData;
+  ngOnInit(): void {
+  }
+
+}
